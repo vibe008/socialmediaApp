8727d35e1f7b90fcca054a5e94ed56822d4fee42
Aryan <aryanbawankar22@gmail.com>
2023-03-21 20:47:48 +0530

    changing que section

--- src/Components/Login/authentication/QueContainer.js
+++ src/Components/Login/authentication/QueContainer.js
@@ -2,32 +2,6 @@ import { StyleSheet, Text, View } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import Questions from './Questions'
 import getQuestions from '../../../Service/question'
-const demoquestionRespData =[
-    {
-      _id: "641196fa4a042cea0207878d",
-      query: "Which one of the following river flows between Vindhyan and Satpura ranges?",
-      op1: "Narmada",
-      op2: "Mahanadi",
-      op3: "Son",
-      op4: "Netravati",
-      ans: "Narmada",
-      status: 1,
-      createdDateTime: "2023-03-15T09:55:53.995Z",
-      __v: 0
-    },
-    {
-      _id: "641197364a042cea0207878f",
-      query: "The Central Rice Research Station is situated in?",
-      op1: "Chennai",
-      op2: "Cuttack",
-      op3: "Bangalore",
-      op4: "Quilon",
-      ans: "Cuttack",
-      status: 1,
-      createdDateTime: "2023-03-15T09:55:53.995Z",
-      __v: 0
-    }
-  ]
 
 const QueContainer = ({navigation}) => {
     const [questionRespData,setQuestionRespData] = useState()
@@ -39,7 +13,6 @@ const QueContainer = ({navigation}) => {
       (async()=>{
         console.log("getQuestions",getQuestions())
         const resp = await getQuestions()
-        console.log(resp.message)
         setQuestionRespData(resp.message)
       })()
     },[])
